01040af5ca1a15f97307661a99aa5ca4bedc5df8
carlaraya <carl_angelo.araya@upd.edu.ph>
2015-09-09 18:31:42 +0800

    Added zoom and switching to mand, but still buggy

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-Julia Set Generator 1.0
+Julia/Mandelbrot Set Generator 1.0.1 (buggy)
 
 
 Features:<br>
--Click on a point in the complex plane and it will generate the julia set of that point.<br>
--Shows coordinates<br>
--Rainbow colors yay
+- Right-click on a point in the complex plane and it will generate the julia set of that point.<br>
+- Left-click to zoom in, press S to zoom out<br>
+- Press m to switch to mandelbrot set, then j to go back to julia sets.<br>
+- Shows coordinates on command line<br>
+- Rainbow colors yay
--- julia.cpp
+++ julia.cpp
@@ -1,3 +1,10 @@
+/**
+ * TODO:
+ * -Print values of every single variable before/after generating fractal
+ * -Fix bugs that show when zooming in and in and in
+ * -Add diversity to colors without reducing maxIter
+ */
+
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
@@ -10,17 +17,38 @@ const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 600;
 
 //Constants for julia set generation
-int pixelsPerUnit = 240;
+long long pixelsPerUnit = 240;
 const double bounds = 2.0;
 const double bounds2 = bounds*bounds;
-const int maxIter = 100;
+const int maxIter = 1000;
 
 double cRe = 0;
 double cIm = 0;
 
 int colors[maxIter+1];
 const int bn = 6;
+//ROYGBI
 int between[][3] = { {150,0,0}, {255,255,0}, {0,255,0}, {0,255,255}, {0,127,255}, {0,0,255} };
+
+int centerX = SCREEN_WIDTH / 2;
+int centerY = SCREEN_HEIGHT / 2;
+
+int pointX = centerX;
+int pointY = centerY;
+
+double centerRe = 0;
+double centerIm = 0;
+
+/*
+//Constants for minimand generation
+int mstartX = SCREEN_WIDTH - 200;
+int mstartY = 10;
+int mcenterX = SCREEN_WIDTH - 100;
+int mcenterY = 110;
+int mendX = 2 * mcenterX - mstartX;
+int mendY = 2 * mcenterY - mstartY;
+int mbarThickness = 10;
+*/
 
 //Starts up SDL and creates window
 bool init();
@@ -35,35 +63,22 @@ SDL_Window* gWindow = NULL;
 SDL_Surface* gScreenSurface = NULL;
 
 //Convert pixel coordinates to complex number
-void coord2z(int x, int y, double* a, double* b);
+void coord2z(int x, int y, int midx, int midy, int pixelsPerUnit, double centerRe, double centerIm, double* a, double* b);
 
 //Put pixel
 void PutPixel32_nolock(SDL_Surface * surface, int x, int y, Uint32 color);
 
+//Make palette from ROYGBI
+void generatePalette();
+
 //Generate julia set
 void generateJulia();
+void generateMandelbrot();
+//void generateMinimand();
 
 int main(int argc, char* args[])
 {
-    //Colors and stuff
-    int ci = 0;
-    int segment = maxIter / (bn - 1);
-    for (int bi = 0; bi < (bn - 1); bi++)
-    {
-        for (int i = 0; i < segment; i++)
-        {
-            int curr[3];
-            colors[ci] = 0;
-            for (int chan = 0; chan < 3; chan++)
-            {
-                curr[chan] = (int)((((double)between[bi+1][chan])- between[bi][chan])/ segment * i)+ between[bi][chan];
-                colors[ci] += curr[chan] << (8 * chan);
-            }
-            //printf("%d %d %d %d\n", curr[0], curr[1], curr[2], ci);
-            ci++;
-        }
-    }
-    colors[maxIter] = 0;
+    generatePalette();
 
 	//Start up SDL and create window
 	if(!init())
@@ -80,12 +95,17 @@ int main(int argc, char* args[])
 
         //Generate julia set
         generateJulia();
+        //generateMinimand();
+
+        SDL_UpdateWindowSurface(gWindow);
+
+        bool mandorjulia = false;
 
         //While application is running
         while(!quit)
         {
             //Handle events on queue
-            while(SDL_PollEvent(&e)!= 0)
+            if (SDL_PollEvent(&e))
             {
                 //User requests quit
                 if(e.type == SDL_QUIT)
@@ -95,16 +115,48 @@ int main(int argc, char* args[])
                 //If mouse clicked, generate julia set
                 else if (e.type == SDL_MOUSEBUTTONDOWN)
                 {
-                    if (e.button.button == SDL_BUTTON_LEFT)
+                    pointX = e.button.x;
+                    pointY = e.button.y;
+                    if (!mandorjulia && e.button.button == SDL_BUTTON_RIGHT)
                     {
-                        coord2z(e.button.x, e.button.y, &cRe, &cIm);
+                        coord2z(pointX, pointY, centerX, centerY, pixelsPerUnit, centerRe, centerIm, &cRe, &cIm);
                         generateJulia();
                         printf("%g %g\n", cRe, cIm);
                     }
+                    else if (e.button.button == SDL_BUTTON_LEFT)
+                    {
+                        pixelsPerUnit *= 2;
+                        coord2z(pointX, pointY, centerX, centerY, pixelsPerUnit, centerRe, centerIm, &centerRe, &centerIm);
+                        if (mandorjulia) generateMandelbrot();
+                        else generateJulia();
+                    }
+                }
+
+                else if (e.type == SDL_KEYDOWN)
+                {
+                    switch (e.key.keysym.sym)
+                    {
+                        case SDLK_m:
+                            mandorjulia = true;
+                            generateMandelbrot();
+                            break;
+                        case SDLK_j:
+                            mandorjulia = false;
+                            generateJulia();
+                            break;
+                        case SDLK_a:
+                            pixelsPerUnit *= 2;
+                            if (mandorjulia) generateMandelbrot();
+                            else generateJulia();
+                            break;
+                        case SDLK_s:
+                            pixelsPerUnit /= 2;
+                            if (mandorjulia) generateMandelbrot();
+                            else generateJulia();
+                            break;
+                    }
                 }
             }
-
-            //Update the surface
             SDL_UpdateWindowSurface(gWindow);
         }
 	}
@@ -160,10 +212,32 @@ void PutPixel32_nolock(SDL_Surface * surface, int x, int y, Uint32 color)
     *((Uint32*)pixel)= color;
 }
 
-void coord2z(int x, int y, double* a, double* b)
+void coord2z(int x, int y, int midx, int midy, int pixelsPerUnit, double centerRe, double centerIm, double* a, double* b)
 {
-    *a = (double)(x - SCREEN_WIDTH / 2)/ pixelsPerUnit;
-    *b = (double)(y - SCREEN_HEIGHT / 2)/ pixelsPerUnit;
+    *a = (double)(x - midx)/ pixelsPerUnit + centerRe;
+    *b = (double)(y - midy)/ pixelsPerUnit + centerIm;
+}
+
+void generatePalette()
+{
+    int ci = 0;
+    int segment = maxIter / (bn - 1);
+    for (int bi = 0; bi < (bn - 1); bi++)
+    {
+        for (int i = 0; i < segment; i++)
+        {
+            int curr[3];
+            colors[ci] = 0;
+            for (int chan = 0; chan < 3; chan++)
+            {
+                curr[chan] = (int)((((double)between[bi+1][chan])- between[bi][chan])/ segment * i)+ between[bi][chan];
+                colors[ci] += curr[chan] << (8 * chan);
+            }
+            //printf("%d %d %d %d\n", curr[0], curr[1], curr[2], ci);
+            ci++;
+        }
+    }
+    colors[maxIter] = 0;
 }
 
 void generateJulia()
@@ -173,19 +247,18 @@ void generateJulia()
         for (int x = 0; x < SCREEN_WIDTH; x++)
         {
             //Do this stuff for every pixel
-            int escaped = 0;
             double a, b;
             double a2, b2;
             double a3, b3;
-            coord2z(x, y, &a, &b);
-            a2 = a;
-            b2 = b;
+            coord2z(x, y, centerX, centerY, pixelsPerUnit, centerRe, centerIm, &a, &b);
+            a2 = a; //a2 = 0; change this line (and the others) to summon mandy
+            b2 = b; //b2 = 0; change this line (and the others) to summon mandy
             int i;
             for (i = 0; i < maxIter; i++)
             {
                 //Math
-                a3 = a2*a2 - b2*b2 + cRe;
-                b3 = 2*a2*b2 + cIm;
+                a3 = a2*a2 - b2*b2 + cRe;//a3 = a2*a2 - b2*b2 + a; change this line (and the others) to summon mandy
+                b3 = 2*a2*b2 + cIm; //b3 = 2*a2*b2 + b; change this line (and the others) to summon mandy
                 a2 = a3;
                 b2 = b3;
                 if (a2 * a2 + b2 * b2 > bounds2)
@@ -198,3 +271,85 @@ void generateJulia()
         }
     }
 }
+
+void generateMandelbrot()
+{
+    for (int y = 0; y < SCREEN_HEIGHT; y++)
+    {
+        for (int x = 0; x < SCREEN_WIDTH; x++)
+        {
+            //Do this stuff for every pixel
+            double a, b;
+            double a2, b2;
+            double a3, b3;
+            coord2z(x, y, centerX, centerY, pixelsPerUnit, centerRe, centerIm, &a, &b);
+            a2 = 0;
+            b2 = 0;
+            int i;
+            for (i = 0; i < maxIter; i++)
+            {
+                //Math
+                a3 = a2*a2 - b2*b2 + a;
+                b3 = 2*a2*b2 + b;
+                a2 = a3;
+                b2 = b3;
+                if (a2 * a2 + b2 * b2 > bounds2)
+                {
+                    break;
+                }
+            }
+            
+            PutPixel32_nolock(gScreenSurface, x, y, colors[i]);
+        }
+    }
+}
+/*
+void generateMinimand()
+{
+    for (int y = mstartY; y < mendY; y++)
+    {
+        for (int x = mstartX; x < mendX; x++)
+        {
+            //Do this stuff for every pixel
+            double a, b;
+            double a2, b2;
+            double a3, b3;
+            coord2z(x, y, mcenterX, mcenterY, 50, &a, &b);
+            a2 = 0;
+            b2 = 0;
+            int i;
+            for (i = 0; i < maxIter; i++)
+            {
+                //Math
+                a3 = a2*a2 - b2*b2 + a;
+                b3 = 2*a2*b2 + b;
+                a2 = a3;
+                b2 = b3;
+                if (a2 * a2 + b2 * b2 > bounds2)
+                {
+                    break;
+                }
+            }
+            
+            PutPixel32_nolock(gScreenSurface, x, y, colors[i]);
+        }
+    }
+    //Draw border
+    for (int y = 0; y < mendY + mbarThickness; y++)
+    {
+        for (int x = mstartX - mbarThickness; x < mstartX; x++)
+        {
+            PutPixel32_nolock(gScreenSurface, x, y, 0);
+            PutPixel32_nolock(gScreenSurface, x + mendX - mstartX, y, 0);
+        }
+    }
+    for (int y = mendY; y < mendY + mbarThickness; y++)
+    {
+        for (int x = mstartX; x < mendX; x++)
+        {
+            PutPixel32_nolock(gScreenSurface, x, y, 0);
+            PutPixel32_nolock(gScreenSurface, x, y - mendY, 0);
+        }
+    }
+}
+*/
